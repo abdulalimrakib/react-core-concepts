8f94c257513e0d39a75769c91ad1d8a2bd5d522c
abdulalimrakib <abdulalimrakib04@gmail.com>
2023-09-12 19:57:25 +0600

    react suru kore dilam finally

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Demo from './components/Demo'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Demo />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/components/Demo.jsx
@@ -0,0 +1,10 @@
+
+const Demo = () => {
+    return (
+        <div>
+            <h2>hello world!!</h2>
+        </div>
+    );
+};
+
+export default Demo;
